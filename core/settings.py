394f031c5a18db9b04c9b4ae9c738a218abe7025
Mahdi <mahdionlineee@gmail.com>
2026-08-06 13:16:22 +0330

    set permissions

--- core/settings.py
+++ core/settings.py
@@ -163,9 +163,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
