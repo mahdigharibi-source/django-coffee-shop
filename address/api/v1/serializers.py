bfb996ed9035c5143000302aaea2f5bc410e2701
Mahdi <mahdionlineee@gmail.com>
2026-08-15 15:43:37 +0330

    refactor address create serializer

--- address/api/v1/serializers.py
+++ address/api/v1/serializers.py
@@ -4,12 +4,18 @@ from address.models import Address
 
 
 class AddressCreateSerializer(serializers.ModelSerializer):
-
+    user_info = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Address
-        exclude = ('created_at','updated_at')
+        exclude = ('user','created_at','updated_at')
+        extra_kwargs = {'user': {'read_only': True}}
 
-        extra_kwargs = {'user':{'read_only':True}}
+    def get_user_info(self, obj):
+        return {
+            'id': obj.user.id,
+            'email': obj.user.email,
+            'phone_number': obj.user.profile.phone_number,
+        }
 
 class AddressUpdateSerializer(serializers.ModelSerializer):
     class Meta:
